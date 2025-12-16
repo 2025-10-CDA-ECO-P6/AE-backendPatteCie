0635c794a28377f56ab89470154a635e9ac35c73
Numazer <saadiemmanuel@macbookpro.home>
2025-12-16 10:36:57 +0100

    rectification server

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import 'dotenv/config';
 
 const app = express();
+
+console.log(process.env.DATABASE_URL);
 
 app.use(express.json());
 
---
+++ src/controllers/authController.js
@@ -0,0 +1,14 @@
+// Register a new user
+export const registerUser = (req, res) => {
+  // Implementation for user registration
+};
+
+// Login an existing user
+export const loginUser = (req, res) => {
+  // Implementation for user login
+};
+
+// Logout a user
+export const logoutUser = (req, res) => {
+  // Implementation for user logout
+};
